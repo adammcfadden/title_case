1dd6e732c3638c275bea35b67111dc40ca671c1b
Toby Alden & Adam McFadden <student@epicodus.com>
2015-04-22 09:58:17 -0700

    add second spec, this tests changing multiple words of a string into title case.

--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -5,4 +5,8 @@ describe('String#title_case') do
   it("given a single word, returns that word with the first letter capitalized and the rest in lowercase") do
     expect("tWO".title_case()).to(eq("Two"))
   end
+
+  it("given multiple words, returns the string with all words in the string having the first letter capitalized and the rest in lowercase.") do
+    expect("tO kIll a MockINgbird".title_case()).to(eq("To Kill A Mockingbird"))
+  end
 end
