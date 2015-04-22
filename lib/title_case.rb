dfc506a0112b44cf13894299b95e8d463cfd3f0b
Toby Alden & Adam McFadden <student@epicodus.com>
2015-04-22 10:30:11 -0700

    Add final spec to make sure that all words are capitalized, except a list of exception words. This also makes sure that the first word is always capitalized. spec passed

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -1,6 +1,16 @@
 class String
   define_method(:title_case) do
-    title = self.capitalize()
-    title  
+    exceptions_array = ["to","the","of","from","and","a","but","or","by","on"]
+    title_array = self.split(" ")
+    title_array.each() do |word|
+      if (exceptions_array.include?(word.downcase()))
+        word.downcase!()
+      else
+        word.capitalize!()
+      end
+    end
+    title_array[0].capitalize!()
+    title = title_array.join(" ")
+    title
   end
 end
--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -6,7 +6,12 @@ describe('String#title_case') do
     expect("tWO".title_case()).to(eq("Two"))
   end
 
-  it("given multiple words, returns the string with all words in the string having the first letter capitalized and the rest in lowercase.") do
-    expect("tO kIll a MockINgbird".title_case()).to(eq("To Kill A Mockingbird"))
+  # it("given multiple words, returns the string with all words in the string having the first letter capitalized and the rest in lowercase, except to, the, of, from, and, but, or, by, on") do
+  #   expect("tO kIll a MockINgbird".title_case()).to(eq("to Kill a Mockingbird"))
+  # end
+
+  it("given multiple words, returns the string with all words in the string having the first letter capitalized and the rest in lowercase, except to, the, of, from, and, a, but, or, by, on. It also makes sure the first word is capitalized") do
+    expect("tO kIll a MockINgbird".title_case()).to(eq("To Kill a Mockingbird"))
   end
+
 end
